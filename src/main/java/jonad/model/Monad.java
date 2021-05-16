e99918ac8751ad81db7a1c9bfc1ef53ab5b7b0d5
Shane <me@shanel.ee>
2021-05-16 20:09:02 +0100

    Update doc comments with return values

--- src/main/java/jonad/model/Monad.java
+++ src/main/java/jonad/model/Monad.java
@@ -16,6 +16,7 @@ public interface Monad<T>
      * a new value from the given function.
      * Does nothing if Monad has no value
      * @param f the mapping function
+     * @return Monad of U
      */
      <U> Monad<U> map(Function<T, U> f);
 
@@ -25,6 +26,7 @@ public interface Monad<T>
      * the result into a single Monad containing the
      * value of the Monad resulting from the function
      * @param f the function resulting in a Monad
+     * @return Monad of U or empty
      */
     <U> Monad<U> flatMap(Function<? super T, ? extends Monad<? extends U>> f);
 
@@ -36,6 +38,7 @@ public interface Monad<T>
      * value of the original Monad. Else an empty
      * Monad will be returned
      * @param f the predicate function
+     * @return Monad of T or empty
      */
     Monad<T> filter(Predicate<? super T> f);
 
@@ -49,22 +52,26 @@ public interface Monad<T>
      * will retain the value of the original Monad.
      * Else an empty Monad will be returned
      * @param f the predicate function
+     * @return Monad of T or empty
      */
     Monad<T> filterWhen(Function<? super T, ? extends Monad<? extends Boolean>> f);
 
     /**
      * If this Monad has a value, return it;
-     * else return undefined
+     * else return null
+     * @return T or null
      */
     @Nullable T getOrNull();
 
     /**
      * Transform this Monad to an {@link java.util.Optional}
+     * @return Optional of T or Optional.empty
      */
     Optional<T> toOptional();
 
     /**
      * Transform this Monad to an {@link Stream}
+     * @return Stream of T or empty Stream
      */
     Stream<T> stream();
 
@@ -73,6 +80,8 @@ public interface Monad<T>
      * else return the non-null value provided
      * as an argument to this method
      * @param t the default value
+     * @return the value of this monad or the
+     * given default value
      */
     T getOrDefault(T t);
 
@@ -80,6 +89,8 @@ public interface Monad<T>
      * If this Monad doesn't have a value, invoke
      * the given supplier and return it's value
      * @param f the supplier function
+     * @return the value of this monad or the
+     * given value from the supplier
      */
     T orElseGet(Supplier<T> f);
 
@@ -88,11 +99,14 @@ public interface Monad<T>
      * invoke the given supplier and throw the
      * error it provides
      * @param f the supplier function
+     * @return the value of this monad or throw the
+     * supplied throwable
      */
     <E extends Throwable> T orElseThrow(Supplier<? extends E> f) throws E;
 
     /**
      * Return true if this Monad is Empty
+     * @return boolean true if empty
      */
     boolean isEmpty();
 
@@ -100,6 +114,7 @@ public interface Monad<T>
      * Perform a side-effect if this Monad doesn't
      * contain a value
      * @param f the function to apply
+     * @return the original monad
      */
     <U> Monad<T> doIfEmpty(Consumer<U> f);
 
@@ -107,6 +122,7 @@ public interface Monad<T>
      * Perform a side-effect if this Monad contains
      * a value
      * @param f the consumer function apply
+     * @return the original monad
      */
     <U> Monad<T> doIfPresent(Consumer<U> f);
 
@@ -114,6 +130,7 @@ public interface Monad<T>
      * Perform a side-effect if this Monad contains
      * an Error
      * @param f the error consumer
+     * @return the original monad
      */
     <E extends Throwable> Monad<T> doOnError(Consumer<E> f);
 
@@ -122,6 +139,7 @@ public interface Monad<T>
      * an Error of the given type
      * @param e the error type to match
      * @param f the error consumer
+     * @return the original monad
      */
     <E extends Throwable> Monad<T> doOnError(Class<E> e, Consumer<? super E> f);
 
@@ -130,6 +148,7 @@ public interface Monad<T>
      * an Error which matches the given predicate function
      * @param p the predicate function
      * @param f the error consumer
+     * @return the original monad
      */
     <U> Monad<T> doOnErrorMatching(Predicate<? super Throwable> p, Consumer<U> f);
 
@@ -138,6 +157,7 @@ public interface Monad<T>
      * error, otherwise retain this Monad
      * @param f the mapping function providing the alternative
      * value
+     * @return a new Monad if the original contained an error
      */
     <E extends Throwable, U> Monad<U> onErrorMap(Function<E, U> f);
 
@@ -148,6 +168,7 @@ public interface Monad<T>
      * @param p the predicate function
      * @param f the mapping function providing the alternative
      * value
+     * @return a new Monad if the original contained a matching error
      */
     <E extends Throwable, U> Monad<U> onErrorMapMatching(Predicate<E> p, Function<E, U> f);
 
@@ -156,6 +177,7 @@ public interface Monad<T>
      * error, otherwise retain this Monad
      * @param f the mapping function providing the alternative
      * Monad
+     * @return a new Monad if the original contained an error
      */
     <E extends Throwable, U> Monad<U> onErrorFlatMap(Function<E, Monad<U>> f);
 
@@ -166,6 +188,7 @@ public interface Monad<T>
      * @param p the predicate function
      * @param f the mapping function providing the alternative
      * Monad
+     * @return a new Monad if the original contained a matching error
      */
     <E extends Throwable, U> Monad<U> onErrorFlatMapMatching(Predicate<E> p, Function<E, Monad<U>> f);
 
@@ -173,6 +196,7 @@ public interface Monad<T>
      * Try and map to an alternative Monad if the given function throws
      * return the original otherwise map the value
      * @param f the mapping function providing the alternative Monad
+     * @return a new Monad if the mapping function threw an error
      */
     <U> Monad<U> tryMap(Function<T, U> f);
 
@@ -180,6 +204,7 @@ public interface Monad<T>
      * Switch to an alternative Monad if the this Monad is empty
      * otherwise retain this value
      * @param u the alternative Monad
+     * @return a new Monad if the original was empty
      */
     <U> Monad<U> switchIfEmpty(Monad<U> u);
 
@@ -187,6 +212,7 @@ public interface Monad<T>
      * Default to an alternative value if the this Monad is empty
      * otherwise retain this value
      * @param u the value to provide to a new Monad
+     * @return a new Monad if the original was empty
      */
     <U> Monad<U> defaultIfEmpty(U u);
 }
